ef31816f5cb511539a87a4c1cb5e76d4131167ff
Maga <ded.hamhoev@gmail.com>
2021-03-08 21:37:01 +0300

    done task Dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  let str = ''
+  //console.log(arr)
+
+  if (Array.isArray(arr)) {
+    arr.forEach(el => {
+
+
+      if (typeof (el) == 'string') {
+        el = el.trim()
+        str += el[0];
+      }
+
+    });
+    str = str.toUpperCase();
+    str = str.split('').sort().join('');
+    
+    return str;
+  } else {
+    return false;
+  }
+
+
 };
